0c9221e2aed04f4c474c4c01e79b20bcd7852861
aygulpardane <aygulparpucu@gmail.com>
2023-10-30 12:33:45 -0600

    make register and login routes - working

--- src/server/db/seed.js
+++ src/server/db/seed.js
@@ -12,15 +12,14 @@ async function seed() {
                 username: 'albert',
                 password: 'bertie99',
                 name: 'Al Bert',
-                location: 'Sidney, Australia'
-                // post: {
-                //     create: {
-                //         authorId: 1234,
-                //         title: "First Post",
-                //         content: "This is my first post. I hope I love writing blogs as much as I love writing them.",
-                //         tags: ["#happy", "#youcandoanything"]
-                //     }
-                // }
+                location: 'Sidney, Australia',
+                posts: {
+                    create: {
+                        title: "First Post",
+                        content: "This is my first post. I hope I love writing blogs as much as I love writing them."
+                        // post_tags: ["#happy", "#youcandoanything"]
+                    }
+                }
             }
         })
 
@@ -29,15 +28,14 @@ async function seed() {
                 username: 'sandra',
                 password: '2sandy4me',
                 name: 'Just Sandra',
-                location: 'Ain\'t tellin\''
-                // post: {
-                //     create: {
-                //         authorId: sandra.id,
-                //         title: "How does this work?",
-                //         content: "Seriously, does this even do anything?",
-                //         tags: ["#happy", "#worst-day-ever"]
-                //     }
-                // }
+                location: 'Ain\'t tellin\'',
+                posts: {
+                    create: {
+                        title: "How does this work?",
+                        content: "Seriously, does this even do anything?"
+                        // post_tags: ["#happy", "#worst-day-ever"]
+                    }
+                }
             }
         })
 
@@ -46,15 +44,14 @@ async function seed() {
                 username: 'glamgal',
                 password: 'soglam',
                 name: 'Joshua',
-                location: 'Upper East Side'
-                // post: {
-                //     create: {
-                //         authorId: glamgal.id,
-                //         title: "Living the Glam Life",
-                //         content: "Do you even? I swear that half of you are posing.",
-                //         tags: ["#happy", "#youcandoanything", "#canmandoeverything"]
-                //     }
-                // }
+                location: 'Upper East Side',
+                posts: {
+                    create: {
+                        title: "Living the Glam Life",
+                        content: "Do you even? I swear that half of you are posing."
+                        // post_tags: ["#happy", "#youcandoanything", "#canmandoeverything"]
+                    }
+                }
             }
         })
 
