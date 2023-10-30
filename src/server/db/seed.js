a28bc67e300d3f793c6ce74c6703b424fb5794e7
aygulpardane <aygulparpucu@gmail.com>
2023-10-30 11:30:42 -0600

    users are rendering

--- src/server/db/seed.js
+++ src/server/db/seed.js
@@ -12,15 +12,15 @@ async function seed() {
                 username: 'albert',
                 password: 'bertie99',
                 name: 'Al Bert',
-                location: 'Sidney, Australia',
-                post: {
-                    create: {
-                        authorId: albert.id,
-                        title: "First Post",
-                        content: "This is my first post. I hope I love writing blogs as much as I love writing them.",
-                        tags: ["#happy", "#youcandoanything"]
-                    }
-                }
+                location: 'Sidney, Australia'
+                // post: {
+                //     create: {
+                //         authorId: 1234,
+                //         title: "First Post",
+                //         content: "This is my first post. I hope I love writing blogs as much as I love writing them.",
+                //         tags: ["#happy", "#youcandoanything"]
+                //     }
+                // }
             }
         })
 
@@ -29,15 +29,15 @@ async function seed() {
                 username: 'sandra',
                 password: '2sandy4me',
                 name: 'Just Sandra',
-                location: 'Ain\'t tellin\'',
-                post: {
-                    create: {
-                        authorId: sandra.id,
-                        title: "How does this work?",
-                        content: "Seriously, does this even do anything?",
-                        tags: ["#happy", "#worst-day-ever"]
-                    }
-                }
+                location: 'Ain\'t tellin\''
+                // post: {
+                //     create: {
+                //         authorId: sandra.id,
+                //         title: "How does this work?",
+                //         content: "Seriously, does this even do anything?",
+                //         tags: ["#happy", "#worst-day-ever"]
+                //     }
+                // }
             }
         })
 
@@ -46,15 +46,15 @@ async function seed() {
                 username: 'glamgal',
                 password: 'soglam',
                 name: 'Joshua',
-                location: 'Upper East Side',
-                post: {
-                    create: {
-                        authorId: glamgal.id,
-                        title: "Living the Glam Life",
-                        content: "Do you even? I swear that half of you are posing.",
-                        tags: ["#happy", "#youcandoanything", "#canmandoeverything"]
-                    }
-                }
+                location: 'Upper East Side'
+                // post: {
+                //     create: {
+                //         authorId: glamgal.id,
+                //         title: "Living the Glam Life",
+                //         content: "Do you even? I swear that half of you are posing.",
+                //         tags: ["#happy", "#youcandoanything", "#canmandoeverything"]
+                //     }
+                // }
             }
         })
 
@@ -62,5 +62,7 @@ async function seed() {
         console.error(error);
     }
 };
+
+seed();
 
 module.exports = seed;
