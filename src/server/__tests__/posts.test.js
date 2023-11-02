28c9ff997c9afe73159291826cf867580a8b2d1f
aygulpardane <aygulparpucu@gmail.com>
2023-11-02 14:29:49 -0600

    add create post test

--- src/server/__tests__/posts.test.js
+++ src/server/__tests__/posts.test.js
@@ -1,6 +1,8 @@
 const {mockDeep} = require('jest-mock-extended');
 const app = require('../app');
 const request = require('supertest');
+jest.mock('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
 const prismaMock = require('../mocks/prismaMock');
 
@@ -33,11 +35,40 @@ describe('GET /posts/:id', () => {
     });
 });
 
-// describe('Create /posts', () => {
-//     it('creates a post for an authenticated user', async () => {
+describe('Create /posts', () => {
+    beforeEach(() => {
+        jest.resetAllMocks();
+    })
+    it('creates a post for an authenticated user', async () => {
+        const user = {
+            id: 123,
+        }
 
-//     });
-// });
+        const newPost = {
+            id: 1,
+            title: "Hat",
+            content: "Selling one fancy hat",
+            authorId: user.id
+        };
+
+        jwt.verify.mockReturnValue({id: user.id})
+        prismaMock.post.create.mockResolvedValue(newPost);
+
+        const response = await request(app)
+            .post('/posts')
+            .set('Authorization', 'Bearer faketesttoken');
+
+        console.log(response.body);
+
+        expect(response.body.id).toEqual(newPost.id);
+        expect(response.body.title).toEqual(newPost.title);
+        expect(response.body.description).toEqual(newPost.description);
+        expect(response.body.price).toEqual(newPost.price);
+        expect(response.body.authorId).toEqual(user.id);
+
+        expect(prismaMock.post.create).toHaveBeenCalledTimes(1);
+    });
+});
 
 // describe('Update /posts/:id', () => {
 //     it('updates a post for an authenticated user', async () => {
