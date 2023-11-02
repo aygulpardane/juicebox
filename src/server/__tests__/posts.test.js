cd821dfc6b55ed7dbd1192bc16b988ffe3bae605
aygulpardane <aygulparpucu@gmail.com>
2023-11-02 15:28:52 -0600

    add update and delete posts tests

--- src/server/__tests__/posts.test.js
+++ src/server/__tests__/posts.test.js
@@ -38,7 +38,7 @@ describe('GET /posts/:id', () => {
 describe('Create /posts', () => {
     beforeEach(() => {
         jest.resetAllMocks();
-    })
+    });
     it('creates a post for an authenticated user', async () => {
         const user = {
             id: 123,
@@ -70,14 +70,76 @@ describe('Create /posts', () => {
     });
 });
 
-// describe('Update /posts/:id', () => {
-//     it('updates a post for an authenticated user', async () => {
+describe('Update /posts/:id', () => {
+    beforeEach(() => {
+        jest.resetAllMocks();
+    });
+    it('updates a post for an authenticated user', async () => {
+        const user = {
+            id: 123,
+        }
 
-//     });
-// });
+        const postToUpdate = {
+            id: 1,
+            title: "Hat",
+            content: "Selling one fancy hat",
+            authorId: user.id
+        };
 
-// describe('Delete /posts/:id', () => {
-//     it('deletes a post for an authenticated user', async () => {
+        const updatedPost =  {
+            id: 1,
+            title: "Pants",
+            content: "Selling fancy pants",
+            authorId: user.id
+        };
 
-//     });
-// });
+        jwt.verify.mockReturnValue({id: user.id});
+        prismaMock.post.findUnique.mockResolvedValue(postToUpdate);
+        prismaMock.post.update.mockResolvedValue(updatedPost);
+
+        const response = await request(app)
+            .patch('/posts/1')
+            .set('Authorization', 'Bearer faketesttoken')
+            .send(updatedPost);
+
+        expect(response.body.id).toEqual(updatedPost.id);
+        expect(response.body.title).toEqual(updatedPost.title);
+        expect(response.body.description).toEqual(updatedPost.description);
+        expect(response.body.price).toEqual(updatedPost.price);
+
+        expect(prismaMock.post.update).toHaveBeenCalledTimes(1);
+    });
+});
+
+describe('Delete /posts/:id', () => {
+    beforeEach(() => {
+        jest.resetAllMocks();
+    });
+    it('deletes a post for an authenticated user', async () => {
+        const user = {
+            id: 123,
+        }
+
+        const postToDelete = {
+            id: 1,
+            title: "Hat",
+            content: "Selling one fancy hat",
+            authirId: user.id
+        };
+
+        jwt.verify.mockReturnValue({id: user.id});
+        prismaMock.post.findUnique.mockResolvedValue(postToDelete);
+        prismaMock.post.delete.mockResolvedValue(postToDelete);
+
+        const response = await request(app)
+            .delete('/posts/1')
+            .set('Authorization', 'Bearer faketesttoken');
+
+        expect(response.body.id).toEqual(postToDelete.id);
+        expect(response.body.title).toEqual(postToDelete.title);
+        expect(response.body.description).toEqual(postToDelete.description);
+        expect(response.body.price).toEqual(postToDelete.price);
+
+        expect(prismaMock.post.delete).toHaveBeenCalledTimes(1);
+    });
+});
